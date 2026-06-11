c355cde2e785462afa4aad7a0e9f40611b9f90ed
Dalmasio <dalma@MAIN-PC>
2026-06-10 22:48:41 -0300

    add entidades

---
+++ FiapGames.PaymentService/2-Payment.Domain/Compra.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain
+{
+    public class Compra
+    {
+        public int Id { get; private set; }
+        public DateTime DataCompra { get; private set; }
+        public decimal ValorTotalBruto { get; private set; }
+        public decimal ValorTotalLiquido { get; private set; }
+
+        public Compra()
+        {
+        }
+
+        public Compra(int id)
+        {
+            Id = id;
+            DataCompra = DateTime.Now;
+        }
+    }
+}
---
+++ FiapGames.PaymentService/2-Payment.Domain/Pagamento.cs
@@ -0,0 +1,38 @@
+﻿namespace Domain
+{
+    public class Pagamento
+    {
+        public int IdPagamento { get; }
+        public int IdCompra { get; }
+        public DateTime DataHoraInclusao { get; }
+        public DateTime? DataHoraAlteracao { get; set; }
+        public STATUS_PAGAMENTO Status { get; set; }
+
+        public Pagamento()
+        {
+            
+        }
+
+        public Pagamento(int idPagamento, int idCompra)
+        {
+            IdPagamento = idPagamento;
+            IdCompra = idCompra;
+            DataHoraInclusao = DateTime.Now;
+            Status = STATUS_PAGAMENTO.AGUARDANDO_PAGAMENTO;
+        }
+
+        public void RecusarPagamento()
+        {
+            DataHoraAlteracao = DateTime.Now;
+            Status = STATUS_PAGAMENTO.RECUSADO;
+        }
+
+        public void ConfirmarPagamento()
+        {
+            DataHoraAlteracao = DateTime.Now;
+            Status = STATUS_PAGAMENTO.CONCLUIDO;
+        }
+
+
+    }
+}
